aa25e4fdea45e2b4ccf3352f798b458b5ce2c022
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2020-03-23 13:24:31 -0400

    disable coverage

--- travis/tests-unit.sh
+++ travis/tests-unit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pytest --cov-append test/unit
+pytest test/unit/test_util_file.py
 
 rc=$?; if [[ $rc != 0 ]]; then sleep 10; exit $rc; fi
 # sleep to allow logs to be printed without truncation in the event of error
